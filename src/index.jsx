04221e8005b54bff2179ef910facd2e1094de261
Fiona0710 <berthou.fiona@hotmail.fr>
2023-09-20 13:11:01 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Error from './components/Error'
+import Footer from './components/Footer'
+import { ThemeProvider, SurveyProvider } from './utils/context'
+import GlobalStyle from './utils/style/GlobalStyle'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/freelances" element={<Freelances />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider> 
+      </ThemeProvider>
+      
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
